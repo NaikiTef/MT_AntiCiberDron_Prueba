02e2fb0531ae14d83b7444c9746ecf5ffcc8943c
NaikiTef <robalinotiffany75@gmail.com>
2026-01-14 22:16:23 -0600

    Add files via upload

---
+++ MTChip.java
@@ -0,0 +1,7 @@
+package MTBussinessComponent.TMEntities;
+
+public class MTChip {
+
+    public MTChip() {
+    }
+}
---
+++ MTZangano.java
@@ -0,0 +1,9 @@
+package MTBussinessComponent.TMEntities;
+
+public class MTZangano {
+
+    public void MTRatrear(String chip) {
+        System.out.println("Zangano: Ratreando con chip " + chip);
+    }
+
+}
